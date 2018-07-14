65746c6a2c4278266f27b70bda8ff9592ea0f6dc
yuxinwen <yuxinwen@lvmama.com>
2018-07-14 23:10:50 +0800

    Initial Commit

---
+++ InjectCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InjectCode
+//
+//  Created by 新闻 on 2018/7/14.
+//  Copyright © 2018年 新闻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InjectCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InjectCode
+//
+//  Created by 新闻 on 2018/7/14.
+//  Copyright © 2018年 新闻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
